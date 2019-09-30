7964261c23b0ca182fb9a946fc4be266defff76f
Marcelo JS Santos <marcelojssantos2012@gmail.com>
2019-09-30 09:08:12 -0300

    Criado operação de instanciação para 'Categoria'

--- src/main/java/com/marcelojssantos/cursomc/CursomcApplication.java
+++ src/main/java/com/marcelojssantos/cursomc/CursomcApplication.java
@@ -1,13 +1,36 @@
 package com.marcelojssantos.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.marcelojssantos.cursomc.domain.Categoria;
+import com.marcelojssantos.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	// método herdado de CommandLineRunner
+	@Override
+	public void run(String... args) throws Exception {
+		
+		// criamos os objetos
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Saúde");
+		
+		//adicionamos uma lista de objetos no BD usando o repository
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 
 }
