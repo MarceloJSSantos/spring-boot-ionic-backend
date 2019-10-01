347e4f11b89718bc3da36a86ff2493f931e2675a
Marcelo JS Santos <marcelojssantos2012@gmail.com>
2019-09-30 21:12:15 -0300

    - Ajusta o endpoint /categorias/{id} - nota: É preciso acertar o tratamento de exceção

--- src/main/java/com/marcelojssantos/cursomc/domain/Produto.java
+++ src/main/java/com/marcelojssantos/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -23,6 +25,7 @@ public class Produto implements Serializable{
 	private String nome;
 	private Double preco;
 	
+	@JsonBackReference // Faz // Para quando for retornar o Json as categorias NÃO são processadas
 	// Iniciamos uma coleção de Categorias para associação * Produto p/ * Categorias
 	@ManyToMany // relacionamento muito(*) p/ muitos(*)
 	// Sendo o mapeamento 'ManyToMany', definimos a tabela que será criada
